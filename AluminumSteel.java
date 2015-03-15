a551469a3388ff74bc815539dc742652be6b341d
proboscisjoe <proboscisjoe@gmail.com>
2015-03-15 11:16:31 -0400

    Unified Steel Type and Cost Operations
    1. renamed “type” to “form factor”
    2. restricted setFormFactor() to operateon null formFactor
    3. added changeFormFactor to operate on non-null formFactor
    4. added parallel data structure to store the “price offset” of each
    type to remove duplicate code. The offset array list is related to the
    form factor array list by a common index.

--- AluminumSteel.java
+++ AluminumSteel.java
@@ -1,11 +1,5 @@
 package buildinglayout;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author xthom001
@@ -14,6 +8,6 @@ public class AluminumSteel extends SteelSuper{
     private static final double  cost =  24.26;
     public AluminumSteel(int amount){
         super(amount,cost);
-        super.setTypes();
+        setFormFactors(); //no need to specify super.methodName() because method is not overridden
     }
 }
--- CarbonSteel.java
+++ CarbonSteel.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package buildinglayout;
 
 /**
@@ -13,6 +8,6 @@ public class CarbonSteel extends SteelSuper{
     private static final double cost = 30.90;
     public CarbonSteel(int amount){
         super(amount,cost);
-        super.setTypes();
+        setFormFactors(); //no need to specify super.methodName() because method is not overridden
     }
 }
